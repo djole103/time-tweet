9797a07f3684baf5ba0b80e951bcdea8536ea1bf
djole <djordje_7@hotmail.com>
2016-03-23 23:14:18 -0400

    loads all tweets, tweets first and rewrites tweets file with that 1 gone

--- main.go
+++ main.go
@@ -18,35 +18,34 @@ func PostTweet(client *twitter.Client, tweet string) error {
 	return nil
 }
 
-func main() {
-	consumer_key := os.Getenv("TWITTER_CONS_KEY")
-	consumer_secret := os.Getenv("TWITTER_CONS_SECRET")
-	access_token := os.Getenv("TWITTER_ACCESS_TOKEN")
-	access_secret := os.Getenv("TWITTER_ACCESS_SECRET")
-	FILENAME := flag.String("tweets", "tweets", "Path to tweets")
-	flag.Parse()
-	var err error
-	var tweet string
-
-	config := oauth1.NewConfig(consumer_key, consumer_secret)
-	token := oauth1.NewToken(access_token, access_secret)
-
-	httpClient := config.Client(oauth1.NoContext, token)
-	client := twitter.NewClient(httpClient)
-	fmt.Println(*FILENAME)
-	tweet, err = LoadTweet(*FILENAME)
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
 	if err != nil {
-		fmt.Println("Failed to load tweet")
-		fmt.Println(err)
+		return nil, err
 	}
-	fmt.Println(tweet)
-	err = CheckValid(tweet)
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// writeLines writes the lines to the given file.
+func writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
 	if err != nil {
-		fmt.Println("Failed to validate tweet")
-		fmt.Println(err)
-		return
+		return err
 	}
-	PostTweet(client, tweet)
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
 
 func LoadTweet(FILENAME string) (string, error) {
@@ -73,4 +72,40 @@ func CheckValid(tweet string) error {
 		return fmt.Errorf("More than 140 chars")
 	}
 	return nil
+}
+
+func main() {
+	consumer_key := os.Getenv("TWITTER_CONS_KEY")
+	consumer_secret := os.Getenv("TWITTER_CONS_SECRET")
+	access_token := os.Getenv("TWITTER_ACCESS_TOKEN")
+	access_secret := os.Getenv("TWITTER_ACCESS_SECRET")
+	FILENAME := flag.String("tweets", "tweets", "Path to tweets")
+	flag.Parse()
+	var err error
+	var tweets []string
+
+	config := oauth1.NewConfig(consumer_key, consumer_secret)
+	token := oauth1.NewToken(access_token, access_secret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+	fmt.Println(*FILENAME)
+	tweets, err = readLines(*FILENAME)
+
+	if err != nil {
+		fmt.Println("Failed to load tweets")
+		fmt.Println(err)
+	}
+	if len(tweets) == 0 {
+		fmt.Println("Refill your tweets dawg")
+		os.Exit(1)
+	}
+	err = CheckValid(tweets[0])
+	if err != nil {
+		fmt.Println("Failed to validate tweet")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	PostTweet(client, tweets[0])
+	writeLines(tweets[1:], *FILENAME)
 }
